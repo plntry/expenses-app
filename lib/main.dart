6a2b7255811c6f0d6eff27fe819a52300daf9c09
Oksana Nedilko <nedilko7b@gmail.com>
2023-02-12 15:29:59 +0200

    added styles for transitions

--- lib/main.dart
+++ lib/main.dart
@@ -48,10 +48,54 @@ class MyHomePage extends StatelessWidget {
             ),
             elevation: 5,
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-          ),
+          Column(
+            children: transactions
+                .map((tx) => Card(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 15,
+                            ),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            )),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              tx.amount.toString(),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tx.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                tx.date.toString(),
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ))
+                .toList(),
+          )
         ],
       ),
     );
