99552af00368ea8308c19be7a40aee240a458e47
Oksana Nedilko <nedilko7b@gmail.com>
2023-02-14 14:13:07 +0200

    added simple fields validation & rounded amounts

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,20 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,18 +34,18 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData,
               // onChanged: (val) => titleInput = val,
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData,
               // onChanged: (val) => amountInput = val,
             ),
             TextButton(
-              onPressed: () => addTx(
-                titleController.text,
-                double.parse(amountController.text),
-              ),
+              onPressed: submitData,
               child: Text('Add Transaction'),
               style: TextButton.styleFrom(
                 foregroundColor: Colors.purple,
